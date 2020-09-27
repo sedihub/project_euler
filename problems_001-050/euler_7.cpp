7b3b57fb01793c882cd8dc6a4311c82fd947bf60
sedigh <luciferous@protonmail.com>
2020-09-27 09:50:27 -0400

    Updated `euler_7.cpp`.

--- problems_001-050/euler_7.cpp
+++ problems_001-050/euler_7.cpp
@@ -11,20 +11,42 @@ SOLUTION:
 #include <set>
 
 template <typename T>
-class IsPrime
+class PrimalityTest
 {
 public:
-    IsPrime(){
+    PrimalityTest()
+    {
         primes.insert(2);
         primes.insert(3);
         primes.insert(5);
     }
 
-    bool test(T n)
+    bool is_prime(T n)
     {
+        if(n == 1)
+            return false;
+        if (primes.find(n) != primes.end())
+            return true;
         for (it = primes.begin(); it != primes.end(); ++it){
-            if(n % (*it) == 0) return false;
-            if((*it) * (*it) > n) break;
+            if (n % (*it) == 0) {
+                return false;
+            }
+            if ((*it) * (*it) > n) {
+                primes.insert(n);
+                return true;
+            }
+        }
+        //
+        // If this number is beyond the range of current primes, get the rest:
+        rit = primes.rbegin();
+        std::advance(rit, 1);
+        T m = (*rit);
+        while (m <= n) {
+            m++;
+            this->is_prime(m);
+            if (n % m == 0) {
+                return false;
+            }
         }
         primes.insert(n);
         return true;
@@ -59,29 +81,29 @@ int main()
 {
     const int nth_prime = 10001;
 
-    IsPrime<unsigned long long int> is_prime;
-    int counter = is_prime.get_number_of_primes();
+    PrimalityTest<unsigned long long int> pt;
+    int counter = pt.get_number_of_primes();
     unsigned long long int k = 6;
     while (counter < nth_prime) {
-        if (is_prime.test(k + 1)) {
+        if (pt.is_prime(k + 1)) {
             counter++;
         }
-        if (is_prime.test(k + 5)) {
+        if (pt.is_prime(k + 5)) {
             counter++;
         }
         k += 6;
     }
 
-    // is_prime.print_primes();
+    // pt.print_primes();
 
     if (counter == nth_prime + 2) {
-        std::cout << is_prime.get_nth_last_prime(2) << std::endl;
+        std::cout << pt.get_nth_last_prime(2) << std::endl;
     }
     else if (counter == nth_prime + 1) {
-        std::cout << is_prime.get_nth_last_prime(1) << std::endl;
+        std::cout << pt.get_nth_last_prime(1) << std::endl;
     }
     else if (counter == nth_prime){
-        std::cout << is_prime.get_nth_last_prime(0) << std::endl;
+        std::cout << pt.get_nth_last_prime(0) << std::endl;
     }
    	else {
    		std::cerr << "ERROR..." << std::endl;
