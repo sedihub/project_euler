0d4faad269735e7e79d7f18bbb080b1e2e4c5595
sedigh <luciferous@protonmail.com>
2020-10-03 12:40:32 -0400

    Modified the src code of the solution of problem 5.

--- problems_001-050/euler_5.cpp
+++ problems_001-050/euler_5.cpp
@@ -1,18 +1,21 @@
 /*
 PROBLEM:
-    What is the smallest positive number that is evenly divisible 
-    by all of the numbers from 1 to 20?
+  What is the smallest positive number that is evenly divisible 
+  by all of the numbers from 1 to 20?
+
 
 SOLUTION:
-    The product of all pure largest primes powers smaller than 20!
-        (2^4) * (3^2) * 5 * 7 * 11 * 13 * 17 * 19 = 232,792,560
+  The product of all pure largest primes powers smaller than 20!
+    (2^4) * (3^2) * 5 * 7 * 11 * 13 * 17 * 19 = 232,792,560
+
+  ANSWER: 232,792,560
 **/
 
 #include <iostream>  
 
 int main()
 {
-    std::cout << 16 * 9 * 5 * 7 * 11 * 13 * 17 * 19 << std::endl;
+  std::cout << 16 * 9 * 5 * 7 * 11 * 13 * 17 * 19 << std::endl;
 
-    return 0;
+  return 0;
 } 
