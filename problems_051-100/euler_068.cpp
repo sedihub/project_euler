68ab4dc9a666ce8485b262c2246e18a3f0836b6d
sedigh <luciferous@protonmail.com>
2020-12-29 13:42:37 -0500

    Modified the solution to 068.

--- problems_051-100/euler_068.cpp
+++ problems_051-100/euler_068.cpp
@@ -1,28 +1,54 @@
-/* Euler 68
- 6,5,3; 10,3,1; 9,1,4; 8,4,2; 7,2,5
- **/
+/*
+PROBLEM:
+  [Problem 68](https://projecteuler.net/problem=68)
+
+SOLUTION:
+  The solution below reveals why we cannot have magic n-gons with an even n. 
+  We break the n-gon into n outer nodes and n inner nodes. The arms will have the arm_sum.
+  Since we use the natural numbers {1, 2,... ,2n} to construct the n-gon, we will have the
+  following equations:
+
+        \sum_{i=1}^{2n}(2I_{i} + O_{i}) = n arm_sum
+                                       = 2 (2n) (2n + 1) /2 - \sum_{i=1}^{2n} O_{i}
+  
+  Now, given an descending set of n numbers in the range {1, 2, ..., 2n}, we can check if
+  these provide a suitable candidate for the outer nodes. If they do, we then try to infer
+  the inner nodes by solving an n-by-n matrix of the form:
+
+        m_{i, j} = \delta_{i, j} + \delta_{i, j+1}    (n+1 = 1)
+
+  Such matrices are only invertible for odd n's! This is why we cannot have magic n-gons 
+  with even n's.
+
+  Note: This can be made much faster by generating candidates and in descending order and 
+  checking them as we go (a depth-first search of sorts)!
+
+  ANSWER: 6 5 3 10 3 1 9 1 4 8 4 2 7 2 5
+**/
 
 #include <iostream>
 #include <vector>
+#include <iterator>
+#include <algorithm>
 
 
 typedef unsigned int uint;
-typedef std::vector<uint> uvec;
+typedef std::vector<uint> UintVec;
 
 
-// std::vector<uvec> generate_ordered_permutations(
-//   std::vector<uvec> prev, uint level, const uint &length, const uint &max)
+// std::vector<UintVec> generate_ordered_permutations(
+//   std::vector<UintVec> prev, uint level, const uint &length, const uint &max)
 // {
 //   if (level > length) {
 //     return prev;
 //   }
 
-//   std::vector<uvec> expanded;
-//   uvec temp_vec;
-//   std::vector<uvec>::iterator it;
+//   std::vector<UintVec> expanded;
+//   UintVec temp_vec;
+//   std::vector<UintVec>::iterator it;
 //   for (it = prev.begin(); it != prev.end(); it++) {
 //     for (uint n = it->back()+1; n <= (max - length + level); n++) {
-//       temp_vec = uvec(*it);
+//       temp_vec = UintVec(*it);
 //       temp_vec.push_back(n);
 //       expanded.push_back(temp_vec);
 //     }
@@ -31,16 +57,16 @@ typedef std::vector<uint> uvec;
 // }
 
 
-std::vector<uvec> generate_nontrivial_permutations(
-  std::vector<uvec> prev, uint level, const uint &length, const uint &max)
+std::vector<UintVec> generate_nontrivial_permutations(
+  std::vector<UintVec> prev, uint level, const uint &length, const uint &max)
 {
   if (level > length) {
     return prev;
   }
 
-  std::vector<uvec> expanded;
-  uvec temp_vec;
-  std::vector<uvec>::iterator vit;
+  std::vector<UintVec> expanded;
+  UintVec temp_vec;
+  std::vector<UintVec>::iterator vit;
   for (vit = prev.begin(); vit != prev.end(); vit++) {
     for (uint n = 1; n <= max; n++) {
       if (std::find(vit->begin(), vit->end(), n) != vit->end()) {
@@ -49,7 +75,7 @@ std::vector<uvec> generate_nontrivial_permutations(
       if (n < (*vit)[0]) {
         continue; 
       }
-      temp_vec = uvec(*vit);
+      temp_vec = UintVec(*vit);
       temp_vec.push_back(n);
       expanded.push_back(temp_vec);
     }
@@ -58,7 +84,7 @@ std::vector<uvec> generate_nontrivial_permutations(
 }
 
 
-bool is_an_ngon_solution(const uint &n_gon, const uvec &outer_nodes, uint arm_sum, uvec &inner_nodes)
+bool is_an_ngon_solution(const uint &n_gon, const UintVec &outer_nodes, uint arm_sum, UintVec &inner_nodes)
 {
   if (n_gon == 3) {
     inner_nodes[0] = (arm_sum - outer_nodes[0] + outer_nodes[1] - outer_nodes[2]) / 2;
@@ -82,11 +108,18 @@ bool is_an_ngon_solution(const uint &n_gon, const uvec &outer_nodes, uint arm_su
     throw;
   }
 
-  uvec::iterator it;
+  // No duplicates in the inner set:
+  if (std::unique(inner_nodes.begin(), inner_nodes.end()) != inner_nodes.end()) {
+    return false;
+  }
+
+  UintVec::iterator it;
   for (it = inner_nodes.begin(); it != inner_nodes.end(); it++) {
+    // inner set is in the expected range:
     if (*it < 1 || *it > 2 * n_gon) {
       return false;
     }
+    // No overlap with the outer set:
     if (std::find(outer_nodes.begin(), outer_nodes.end(), *it) != outer_nodes.end()) {
       return false;
     }
@@ -99,46 +132,68 @@ bool is_an_ngon_solution(const uint &n_gon, const uvec &outer_nodes, uint arm_su
 int main()
 {
   const uint n_gon = 5; // This is the only parameter to change:
-
+  const bool verbose = false;
   const uint max_num = 2 * n_gon;
   const uint sum_one_to_max = n_gon * (max_num + 1);
 
-  std::vector<uvec> perms;
+  UintVec solution;  
+
+  std::vector<UintVec> perms;
   for (uint n = 1; n <= max_num; n++) {
-    perms.push_back(uvec({n}));
+    perms.push_back(UintVec({n}));
   }
   perms = generate_nontrivial_permutations(perms, 2, n_gon, max_num);
-  std::vector<uvec>::iterator vit;
+  std::vector<UintVec>::iterator vit;
   uint sum, arm_sum;
   for (vit = perms.begin(); vit != perms.end(); vit++) {
     sum = 0; 
-    std::cout<< "\t";
-    for (uvec::iterator it = vit->begin(); it != vit->end(); it++) {
+    if (verbose) { std::cout<< "\t"; }
+    for (UintVec::iterator it = vit->begin(); it != vit->end(); it++) {
       sum += *it;
-      std::cout << *it << " ";
+      if (verbose) { std::cout << *it << " "; }
     }
     if ((2 * sum_one_to_max - sum) % n_gon == 0) {
       arm_sum = (2 * sum_one_to_max - sum) / n_gon;
-      std::cout << " ---> [" << arm_sum;
-      uvec inner_nodes(n_gon);
+      if (verbose) { std::cout << " ---> [" << arm_sum; }
+      UintVec inner_nodes(n_gon);
       if (is_an_ngon_solution(n_gon, *vit, arm_sum, inner_nodes)) {
-        std::cout << "]   " ;
+        solution.clear();
+        if (verbose) { std::cout << "]   " ; }
         for (uint idx = 0; idx < n_gon - 1; idx++) {
-          std::cout << (*vit)[idx] << "," << inner_nodes[idx] << "," << inner_nodes[idx+1] << "; "; 
+          solution.push_back((*vit)[idx]);
+          solution.push_back(inner_nodes[idx]);
+          solution.push_back(inner_nodes[idx + 1]);
+          if (verbose) { 
+            std::cout << (*vit)[idx] << "," 
+                      << inner_nodes[idx] << "," 
+                      << inner_nodes[idx+1] << "; "; 
+          }
         }
-        std::cout << (*vit)[n_gon - 1] << "," << inner_nodes[n_gon - 1] << "," << inner_nodes[0];
-        std::cout << std::endl;
+        solution.push_back((*vit)[n_gon - 1]);
+        solution.push_back(inner_nodes[n_gon - 1]);
+        solution.push_back(inner_nodes[0]);
+        if (verbose) { 
+          std::cout << (*vit)[n_gon - 1] << "," 
+                    << inner_nodes[n_gon - 1] << "," 
+                    << inner_nodes[0] << std::endl;
+        }
       }
       else {
-        std::cout << "]" << std::endl;
+        if (verbose) { std::cout << "]" << std::endl; }
       }
     }
     else {
-      std::cout << "\r"; // Earse the line...
+      std::cout << "\r"; // Erase the line...
     }
   }
 
-  // Since the base sequences are generated in lexicographical order, the last solution is the answer!
+  // Since the base sequences are generated in lexicographical order, 
+  // the last solution is the answer!
+  std::cout << "Solution: ";
+  for (uint idx = 0; idx < solution.size(); idx++) {
+    std::cout << solution[idx] << " ";
+  }
+  std::cout << std::endl;
 
   return 0;
 }
